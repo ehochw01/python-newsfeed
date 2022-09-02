03578b51caa96b7c3d3fddb1a9a23c77c72da516
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-09-02 13:51:26 -0400

    completed database setup

---
+++ app/models/User.py
@@ -0,0 +1,28 @@
+from app.db import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+import bcrypt
+
+salt = bcrypt.gensalt()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+
+    @validates('email')
+    def validate_email(self, key, email):
+        # make sure email address contains @ character
+        # The validate_email() method uses the assert keyword to check if an email address contains an at-sign character (@). The assert keyword automatically throws an error if the condition is false, thus preventing the return statement from happening.
+        assert '@' in email
+
+        return email
+
+    @validates('password')
+    def validate_password(self, key, password):
+        assert len(password) > 4
+
+        # encrypt password
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
