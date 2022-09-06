576e9273e20e700b51de39db46685106df9618a2
Eric Hochwald <erichochwald@Erics-MBP.lan>
2022-09-06 16:00:23 -0400

    completed the homepage templates

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+# flask creates a new context every time a server request is made. When the request ends, the context is removed from the app. These temporary contexts provide global variables, like the g object, that can be shared across modules as long as the context is still active.
+from flask import g
 
 load_dotenv()
 
@@ -10,3 +12,27 @@ load_dotenv()
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def init_db(app):
+    # creates any tables that Base mapped in a class that inherits Base (i.e. User)
+    Base.metadata.create_all(engine)
+    # The close_db() function won't run automatically. We need to tell Flask to run it whenever a context is destroyed.
+    app.teardown_appcontext(close_db)
+
+# The get_db() function now saves the current connection on the g object, if it's not already there. Then it returns the connection from the g object instead of creating a new Session instance each time.
+def get_db():
+  if 'db' not in g:
+    # store db connection in app context
+    g.db = Session()
+
+  return g.db
+
+def close_db(e=None):
+  db = g.pop('db', None)
+
+  if db is not None:
+    db.close()
+
+# def get_db():
+#     # Whenever this function is called, it returns a new session-connection object. 
+#     return Session()
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,13 +1,23 @@
 from flask import Blueprint, render_template
+from app.models import Post
+from app.db import get_db
 
 # Blueprint() lets us consolidate routes onto a single bp object that the parent app can register later. This corresponds to using the Router middleware of Express.js.
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
 # we add a @bp.route() decorator before the function to turn it into a route
+# @bp.route('/')
+# def index():
+#   return render_template('homepage.html')
+
 @bp.route('/')
 def index():
-  return render_template('homepage.html')
+  # get all posts
+  db = get_db()
+  posts = db.query(Post).order_by(Post.created_at.desc()).all()
+
+  return render_template('homepage.html', posts=posts)
 
 @bp.route('/login')
 def login():
@@ -15,4 +25,13 @@ def login():
 
 @bp.route('/post/<id>')
 def single(id):
-  return render_template('single-post.html')
+  # get single post by id
+  db = get_db()
+  # use the filter() method on the connection object to specify the SQL WHERE clause
+  post = db.query(Post).filter(Post.id == id).one()
+
+  # pass the single post object to the single-post.html template to render it
+  return render_template(
+    'single-post.html',
+    post=post
+  )
